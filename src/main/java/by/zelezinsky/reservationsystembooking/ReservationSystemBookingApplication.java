efd3606361af7535c030583a16da5e5b10f6ea6a
S1mpleKnight <s1mpleknight@mail.ru>
2023-04-04 20:51:42 +0300

    new: init project

---
+++ src/main/java/by/zelezinsky/reservationsystembooking/ReservationSystemBookingApplication.java
@@ -0,0 +1,13 @@
+package by.zelezinsky.reservationsystembooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSystemBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/zelezinsky/reservationsystembooking/ReservationSystemBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.zelezinsky.reservationsystembooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
