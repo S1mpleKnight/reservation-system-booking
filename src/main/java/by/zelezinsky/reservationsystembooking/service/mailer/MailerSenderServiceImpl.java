fa9cda4365412c600b5ee93fb90f1bb5f55246f2
S1mpleKnight <s1mpleknight@mail.ru>
2023-04-26 02:06:58 +0300

    fix: add message sent logging

--- src/main/java/by/zelezinsky/reservationsystembooking/service/mailer/MailerSenderServiceImpl.java
+++ src/main/java/by/zelezinsky/reservationsystembooking/service/mailer/MailerSenderServiceImpl.java
@@ -1,12 +1,14 @@
 package by.zelezinsky.reservationsystembooking.service.mailer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MailerSenderServiceImpl implements MailerSenderService {
 
     private final JavaMailSender mailSender;
@@ -17,6 +19,12 @@ public class MailerSenderServiceImpl implements MailerSenderService {
         message.setTo(recipient);
         message.setSubject(subject);
         message.setText(body);
-        mailSender.send(message);
+        try {
+            mailSender.send(message);
+            log.info("Message was successfully sent to the ".concat(recipient));
+        } catch (Exception e) {
+            log.error("Message sending failed: ".concat(e.getMessage()));
+        }
+
     }
 }
